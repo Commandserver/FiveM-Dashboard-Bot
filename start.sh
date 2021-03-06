a04cebabbd6d3864ea4e39cd4dc02010ae897b79
Phil B <info@commandserver.org>
2021-03-06 18:49:49 +0100

    Create start.sh
    A bash script to start the Bot

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Starts the bot in a screen
+
+name="fivem-dashboard"
+
+if screen -list | grep $name; then
+	echo "${name} is already online"
+else
+	screen -A -ln -dmS $name bash -c 'python3 pardonbot.py'
+	echo "Discord bot: ${name} started!"
+fi
